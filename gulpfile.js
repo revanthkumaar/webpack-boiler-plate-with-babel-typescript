c529c7686470bcc85fb3d242671223f3da7a1970
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-30 11:02:05 +0530

    gulp checin

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,15 @@ var paths = {
     main_js: ['src/app.tsx']
 }
 
-gulp.task('js',function(){
-    return browserify(paths.main_js).transform(babelify).bundle().on('error',(err) => {console.log(err)})
-    .pipe(source(bundle.js)).pipe(gulp.dest('static/js'))
+gulp.task('js', function() {
+//Browserify bundles the JS.
+return browserify(paths.main_js)
+.transform(babelify) //———–> transpiles es6 to es5
+.bundle()
+.on('error', (err)=>{
+console.log('js error', err);
 })
+.pipe(source('bundle.js'))
+.pipe(gulp.dest('static/js'));
+});
 
